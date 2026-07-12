ab4108195cb75b287b0a223581f5a668fe19e427
HeTreeTerr <3110708879@qq.com>
2026-07-12 13:25:04 +0800

    jvm zgc

---
+++ src/main/java/com/hss/newfeatures/ZgcJvmDemo.java
@@ -0,0 +1,17 @@
+package com.hss.newfeatures;
+
+/**
+ * <p>
+ *
+ * </p>
+ *
+ * @author Hss
+ * @date 2026-07-12
+ */
+public class ZgcJvmDemo {
+
+    /*
+    jdk17 默认使用G1垃圾回收器
+    高并发下推荐切换使用zgc垃圾回收器
+     */
+}
