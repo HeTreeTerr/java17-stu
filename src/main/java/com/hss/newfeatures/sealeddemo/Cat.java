c586d920ec6a269a6f3f30ac7dc22635cda0f5ae
HeTreeTerr <3110708879@qq.com>
2026-07-12 12:11:41 +0800

    jdk新特性

--- src/main/java/com/hss/newfeatures/sealeddemo/Cat.java
+++ src/main/java/com/hss/newfeatures/sealeddemo/Cat.java
@@ -9,4 +9,13 @@ package com.hss.newfeatures.sealeddemo;
  * @date 2026-07-12
  */
 public final class Cat extends Animal{
+
+    /**
+     * 获取名称
+     * @return
+     */
+    @Override
+    public String getName(){
+        return "Cat";
+    }
 }
